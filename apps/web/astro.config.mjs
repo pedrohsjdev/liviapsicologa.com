1abcc2b2614cac7439d4996cc78bf7abe1ae3665
Pedro Henrique <pedrohsj.dev@gmail.com>
2026-01-30 02:32:26 -0300

    feat: add hero page

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,11 +2,13 @@ import node from "@astrojs/node";
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({ mode: "standalone" }),
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
